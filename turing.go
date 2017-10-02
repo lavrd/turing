58cc99fe9a5d192493583324697cef2047f98b7e
spacelavr <spacelavr@users.noreply.github.com>
2017-10-02 12:26:34 +0300

    check unknown characters

--- turing.go
+++ turing.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"flag"
-	"os"
-	"log"
 	"bufio"
-	"strings"
-	"strconv"
 	"errors"
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -18,27 +18,37 @@ const (
 )
 
 type Alphabet []string
-
 type Tape []string
+type Program map[State]map[Head]*Command
 
-type NState struct {
-	nHead  Head
-	nState State
-	Move   int
+type Command struct {
+	nHead      Head
+	nState     State
+	transition int
 }
-
 type Head string
-
 type State int
 
-type Instructions map[State]map[Head]*NState
+type wLog struct {
+	TapeBefore  Tape
+	TapeAfter   Tape
+	HeadBefore  Head
+	HeadAfter   Head
+	StateBefore State
+	StateAfter  State
+	Cmd         Command
+}
 
 func main() {
 
 	var (
-		alphabetPath     = flag.String("alph", "", "path to alphabet file")
-		tapePath         = flag.String("tape", "", "path to tape file")
-		instructionsPath = flag.String("inst", "", "path to instructions file")
+		alphabetPath = flag.String("alph", "", "path to alphabet file")
+		tapePath     = flag.String("tape", "", "path to tape file")
+		programPath  = flag.String("prog", "", "path to program file")
+
+		//savePath = flag.String("save", "", "if the flag is specified logs will be saved to specified file")
+		//verbose  = flag.Bool("v", false, "verbose output")
+		//display = flag.Bool("d", false, "display result")
 	)
 
 	flag.Parse()
@@ -47,14 +57,36 @@ func main() {
 		log.Fatalf("Incorrect alphabet file path: [%s]", *alphabetPath)
 	} else if *tapePath == "" {
 		log.Fatalf("Incorrect tape filepath: [%s]", *tapePath)
-	} else if *instructionsPath == "" {
-		log.Fatalf("Incorrect instructions file path: [%s]", *instructionsPath)
+	} else if *programPath == "" {
+		log.Fatalf("Incorrect program file path: [%s]", *programPath)
+	}
+
+	alphabet, err := LoadAlphabet(*alphabetPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = LoadTape(*tapePath, alphabet)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = LoadProgram(*programPath, alphabet)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func LoadTape(tapePath string) (*Tape, error) {
+func (wl *wLog) Log(savePath string, verbose bool) (bool, error) {
+	return true, nil
+}
 
-	var tape = &Tape{}
+func LoadTape(tapePath string, alphabet *Alphabet) (*Tape, error) {
+
+	var (
+		ok   = false
+		tape = &Tape{}
+	)
 
 	f, err := os.Open(tapePath)
 	if err != nil {
@@ -63,8 +95,22 @@ func LoadTape(tapePath string) (*Tape, error) {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		*tape = strings.Split(scanner.Text(), "")
+	}
+
+	for _, t := range *tape {
+		for _, a := range *alphabet {
+			if t == a {
+				ok = true
+			}
+		}
+
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("LoadTape: unknown character: [%s]", t))
+		}
+		ok = false
 	}
 
 	return tape, nil
@@ -81,6 +127,7 @@ func LoadAlphabet(alphabetPath string) (*Alphabet, error) {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		*alphabet = strings.Split(scanner.Text(), " ")
 	}
@@ -88,83 +135,106 @@ func LoadAlphabet(alphabetPath string) (*Alphabet, error) {
 	return alphabet, nil
 }
 
-func LoadInstructions(instructionsPath string) (*Instructions, error) {
+func LoadProgram(programPath string, alphabet *Alphabet) (*Program, error) {
 
 	var (
-		instructions = make(Instructions)
+		program = make(Program)
 
-		line        = 0
-		instruction []string
-		cStateInt   int
-		nStateInt   int
+		lineIndex = 0
+		line      []string
 
-		cState State
-		nState State
-		cHead  Head
-		nHead  Head
-		move   int
+		cStateInt int
+		nStateInt int
+
+		cState     State
+		nState     State
+		cHead      Head
+		nHead      Head
+		transition int
+
+		ok = false
 	)
 
-	f, err := os.Open(instructionsPath)
+	f, err := os.Open(programPath)
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("LoadInstructions: open file: [%s] err: [%s]", instructionsPath, err))
+		return nil, errors.New(fmt.Sprintf("LoadProgram: open file: [%s] err: [%s]", programPath, err))
 	}
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
-		line++
+		lineIndex++
 
-		instruction = strings.Split(scanner.Text(), "")
+		line = strings.Split(scanner.Text(), "")
 
-		if len(instruction) == 0 || instruction[0] == "#" {
+		if len(line) == 0 || line[0] == "#" {
 			continue
 		}
 
-		if len(instruction) != 7 {
-			return nil, errors.New(fmt.Sprintf("LoadInstructions: parse instruction err: incorrect line [%s] line number: [%d]", instruction, line))
+		if len(line) != 7 {
+			return nil, errors.New(fmt.Sprintf("LoadProgram: parse program err: incorrect line [%s] line number: [%d]", line, lineIndex))
 		}
 
-		cStateInt, err = strconv.Atoi(instruction[0])
+		cStateInt, err = strconv.Atoi(line[0])
 		if err != nil {
-			return nil, errors.New(fmt.Sprintf("LoadInstructions: parse instruction err: convert current state to int err: [%s]", err))
+			return nil, errors.New(fmt.Sprintf("LoadProgram: parse program err: convert current state to int err: [%s]", err))
 		}
 		cState = State(cStateInt)
-		cHead = Head(instruction[1])
 
-		switch instruction[6] {
+		ok = false
+		for _, a := range *alphabet {
+			if a == line[1] {
+				ok = true
+			}
+		}
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("LoadProgram: unknown character: [%s] line: [%d]", line[1], lineIndex))
+		}
+		cHead = Head(line[1])
+
+		switch line[6] {
 		case ">":
-			move = RIGHT
+			transition = RIGHT
 			break
 		case "<":
-			move = LEFT
+			transition = LEFT
 			break
 		case "!":
-			move = STOP
+			transition = STOP
 			break
 		default:
-			return nil, errors.New(fmt.Sprintf("LoadInstructions: parse instruction err: parse head move err: incorrect symbol: [%s]", instruction[7]))
+			return nil, errors.New(fmt.Sprintf("LoadProgram: parse program err: parse head move err: incorrect symbol: [%s]", line[7]))
 		}
 
-		nStateInt, err = strconv.Atoi(instruction[4])
+		nStateInt, err = strconv.Atoi(line[4])
 		if err != nil {
-			return nil, errors.New(fmt.Sprintf("LoadInstructions: convert nezt state to int err: [%s]", err))
+			return nil, errors.New(fmt.Sprintf("LoadProgram: convert next state to int err: [%s]", err))
 		}
 		nState = State(nStateInt)
-		nHead = Head(instruction[5])
 
-		instructions[cState] = make(map[Head]*NState)
-		instructions[cState][cHead] = &NState{
-			nState: nState,
-			Move:   move,
-			nHead:  nHead,
+		ok = false
+		for _, a := range *alphabet {
+			if a == line[5] {
+				ok = true
+			}
+		}
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("LoadProgram: unknown character: [%s] line: [%d]", line[5], lineIndex))
+		}
+		nHead = Head(line[5])
+
+		program[cState] = make(map[Head]*Command)
+		program[cState][cHead] = &Command{
+			nState:     nState,
+			transition: transition,
+			nHead:      nHead,
 		}
 	}
 
-	if len(instruction) == 0 {
-		return nil, errors.New(fmt.Sprint("LoadInstructions: empty instructions"))
+	if len(program) == 0 {
+		return nil, errors.New(fmt.Sprint("LoadProgram: load programm err: empty program"))
 	}
 
-	return &instructions, nil
+	return &program, nil
 }
